c9a767127d885b8c242ce80557dc9278a8b1750a
Stacy <stacy@altmetric.com>
2020-02-14 08:09:38 -0600

    flask app dates endpoints

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
 
+import datetime as dt
+
 # Database / ORM setup
 
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
@@ -35,7 +37,8 @@ def home():
         f"/api/v1.0/stations - Measurement stations<br/>"
         f"/api/v1.0/tobs - Temperature observations (F)<br/>"
         f"/api/v1.0/*start* - Minimum, Average, and Max temps for a date range starting on *start*<br/>"
-        f"/api/v1.0/*start*/*end* - Minimum, Average, and Max temps for a date range starting on *start* & ending on *end*"
+        f"/api/v1.0/*start*/*end* - Minimum, Average, and Max temps for a date range starting on *start* & ending on *end*<br/>"
+        f"Note: Dates should be formatted YYY-MM-DD"
     )
 
 @app.route('/api/v1.0/precipitation')
@@ -93,6 +96,57 @@ def tobs():
         t.append(date)
         t.append(tobs)
         d.append(t)      
+        
+    return jsonify(d)
+
+
+#  `/api/v1.0/<start>` and `/api/v1.0/<start>/<end>`
+
+# Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
+
+@app.route('/api/v1.0/<start_date>')
+def date(start_date):
+    
+    session = Session(engine)
+
+    #   * When given the start only, calculate `TMIN`, `TAVG`, and `TMAX` for all dates greater than and equal to the start date.
+    temps = session.query(Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)) \
+            .filter(Measurement.date >= start_date).group_by(Measurement.date).all()
+
+    session.close()
+
+    d = {}
+
+    for date, min_tobs, avg_tobs, max_tobs in temps:
+        tobs = {}
+        tobs['TMIN'] = min_tobs
+        tobs['TAVG'] = avg_tobs
+        tobs['TMAX'] = max_tobs
+        d[date] = tobs
+        
+    return jsonify(d)
+
+@app.route('/api/v1.0/<start_date>/<end_date>')
+def two_date(start_date, end_date):
+    
+    session = Session(engine)
+    
+    '''When given the start and the end date, calculate the `TMIN`, `TAVG`, 
+    and `TMAX` for dates between the start and end date inclusive.'''
+
+    temps = session.query(Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)) \
+            .filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).group_by(Measurement.date).all()
+
+    session.close()
+
+    d = {}
+
+    for date, min_tobs, avg_tobs, max_tobs in temps:
+        tobs = {}
+        tobs['TMIN'] = min_tobs
+        tobs['TAVG'] = avg_tobs
+        tobs['TMAX'] = max_tobs
+        d[date] = tobs
         
     return jsonify(d)
     
